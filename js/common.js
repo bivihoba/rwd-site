f4783e300961c286507f4d13a93f07005efb47ef
bivihoba <bivihoba@ya.ru>
2014-04-11 02:45:51 +0600

    native semantic

--- js/common.js
+++ js/common.js
@@ -1,57 +1,36 @@
-
-$.fn.cut = function(aParams){
-	var aOptions = {
-		expandSelector: 	getOption('expandSelector', '.b-link_action_expand'),
-		collapseSelector: 	getOption('collapseSelector', '.b-link_action_collapse'),
-		expandClass: 		getOption('expandClass', 'b-cut_state_expanded'),
-		collapseClass: 		getOption('collapseClass', 'b-cut_state_collapsed'),
-		dualClass: 			getOption('dualClass', 'b-cut__action_type_dual'),
-		type: 				getOption('typeClass', 'b-cut_type_addition')
-	}
+$.fn.cut = function(){
 
 	var cutBlock = $(this);
 
-	cutBlock.find(aOptions.collapseSelector).first().click(collapse);
+	cutBlock.find('header').first().click(function(event) {
 
-	cutBlock.find(aOptions.expandSelector).first().click(function(event) {
-		if ($(this).hasClass(aOptions.dualClass)) {
-			if ($(this).closest(cutBlock).hasClass(aOptions.expandClass)) {
+			if ($(this).closest(cutBlock).data('state') === 'opened') {
 				collapse();
 			}
 			else {
 				expand();
 			}
-		}
-		else {
-			expand();
-		}
+
+	});
+
+	cutBlock.find('span[role="presentation"]').first().click(function(event) {
+		event.stopPropagation();
+		$(this).closest('article[data-content="promo"]').data('state', 'off');
+		$(this).closest('article[data-content="promo"]').attr('data-state', 'off');
+
 	});
 
 	function expand() {
-		cutBlock.addClass(aOptions.expandClass).removeClass(aOptions.collapseClass);
-		cutBlock.trigger('cut');
+		cutBlock.data('state', 'opened');
+		cutBlock.attr('data-state', 'opened');
 	}
 
 	function collapse() {
-		cutBlock.addClass(aOptions.collapseClass).removeClass(aOptions.expandClass);
-		cutBlock.trigger('cut');
-	}
-	function getOption(sName, sDefault) {
-		if (typeof(aParams) == 'undefined' || typeof(aParams[sName]) == 'undefined') {
-			return sDefault;
-		}
-		return aParams[sName];
+		cutBlock.data('state', 'collapsed');
+		cutBlock.attr('data-state', 'collapsed');
 	}
 }
 
-
 $(function(){
-	$('.b-cut').cut();
-
-	$('.b-popup__close').click(function(event) {
-		event.preventDefault();
-
-		$(this).closest('.b-popup').removeClass("b-popup_state_opened");
-	});
-
+	$('article[data-content="promo"]').cut();
 });
